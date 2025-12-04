5f254070b9759a65b5fe97c0dc830d76da05fc0c
YinYanCat <chyanez2023@udec.cl>
2025-12-04 16:04:13 -0300

    [FEAT]: DeepSarsa + QNetwork

--- src/DeepSarsa.py
+++ src/DeepSarsa.py
@@ -1,27 +1,60 @@
+from src.QNetwork import QNetwork
+import numpy as np
+import torch
+import torch.nn as nn
+
 class DeepSarsa:
-    def __init__(self, state_dim, action_dim, learning_rate, discount_factor, environment):
-        agent = DQNagent(state_dim, action_dim, learning_rate, discount_factor)
+    def __init__(self, learning_rate, discount_factor, environment):
         self.env = environment
+        self.discount_factor = discount_factor
+        self.learning_rate = learning_rate
+        state_dim, action_dim = self.env.get_dim()
+        w, h = self.env.get_img_size()
+        self.qnet = QNetwork(state_dim,action_dim,w,h)
+        self.qnet_optim = torch.optim.Adam(self.qnet.parameters(),)
+        self.mse_loss_function = nn.MSELoss()
+
+    def epsilon_greedy_action(self, img, state, epsilon=0.5):
+        if np.random.uniform(0, 1) < epsilon:
+                return self.env.action_space.sample()
+        else:
+            network_output_to_numpy = self.qnet(img, state).data.numpy()
+            return np.argmax(network_output_to_numpy)
+
+    def update(self, img, state, next_img, next_state, action, next_action, reward, end):
+        action_tensor = torch.tensor([action], dtype=torch.long)
+        reward_tensor = torch.tensor([reward], dtype=torch.float)
+        end_tensor = torch.tensor([end], dtype=torch.float)
+        next_action_tensor = torch.tensor([next_action], dtype=torch.long)
+
+        q_values = self.qnet(img, state)
+        q_value = q_values.gather(1, action_tensor.unsqueeze(1)).squeeze(1)
+
+        next_q_values = self.qnet(next_img, next_state)
+        next_q_value = next_q_values.gather(1, next_action_tensor.unsqueeze(1)).squeeze(1)
+
+        target = reward_tensor + (1-end_tensor) * self.discount_factor * next_q_value
+        q_network_loss = self.mse_loss_function(q_value, target.detach())
+
+        self.qnet_optim.zero_grad()
+        q_network_loss.backward()
+        self.qnet_optim.step()
 
     def train(self, n_episodes, n_steps):
         for eps in range(n_episodes):
-            state_0 = ...
-            action = ...
-            total_reward = ...
-            stop = ...
-            if stop:
-                pass
-            else:
-                for step in range(n_steps):
-                    state_1 = ...
-                    curr_reward = ...
-                    stop = ...
-                    if stop:
+            state_img, state = self.env.reset()
+            action = self.epsilon_greedy_action(state_img, state)
 
-                        break
+            for step in range(n_steps):
+                next_state_img, next_state, reward, end = self.env.step(action)
+                next_action = self.epsilon_greedy_action(next_state_img,next_state)
 
-            agent.update()
+                self.update(state_img, state, next_state_img, next_state, action, next_action, reward, end)
 
+                state_img, state = next_state_img, next_state
+                action = next_action
+                if end:
+                    break
 
 
 
--- src/QNetwork.py
+++ src/QNetwork.py
@@ -2,29 +2,37 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+def conv2d_size_out(size, kernel_size=1, stride=1, padding=0, dilation=1):
+    return ((size + 2 * padding - dilation * (kernel_size - 1) - 1) // stride) + 1
+
+
 class QNetwork(nn.Module):
-    def __init__(self, state_dim, action_dim):
+    def __init__(self, state_dim, action_dim, image_w, image_h):
         super().__init__()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # CNN para imagen
-        out1_cnn = ...
-        out2_cnn = ...
-        out3_cnn = ...
-        self.conv1 = nn.Conv2d(3, out1_cnn,kernel_size=..., stride=...)
-        self.conv2 = nn.Conv2d(out1_cnn, out2_cnn)
-        self.conv3 = nn.Conv2d(out2_cnn, out3_cnn)
+        out1_cnn = 8
+        out2_cnn = 16
+        out3_cnn = 32
+        self.conv1 = nn.Conv2d(3, out1_cnn,kernel_size=4, stride=2, dilation=1, padding=0)
+        self.conv2 = nn.Conv2d(out1_cnn, out2_cnn,kernel_size=2, stride=2, dilation=1, padding=0)
+        self.conv3 = nn.Conv2d(out2_cnn, out3_cnn,kernel_size=1, stride=1, dilation=1, padding=0)
 
-        self.cnn_output_dim = ... # pixeles de la imagen
+        w_out = conv2d_size_out(conv2d_size_out(conv2d_size_out(image_w, 4, 2), 2, 2), 1, 1)
+        h_out = conv2d_size_out(conv2d_size_out(conv2d_size_out(image_h, 4, 2), 2, 2), 1, 1)
+        self.cnn_output_dim = out3_cnn * w_out * h_out
 
         #  Multilayer Perceptron para inputs continuos
-        self.fc_state1 = nn.Linear(state_dim, ...)
-        self.fc_state2 = nn.Linear()
+        self.fc_state1 = nn.Linear(state_dim, 128)
+        self.fc_state2 = nn.Linear(128,128)
 
         # MLP final para cnn y estado
-        self.fc1 = nn.Linear()
-        self.out = nn.Linear()
+        self.fc1 = nn.Linear(self.cnn_output_dim + 128, 512)
+        self.out = nn.Linear(512, action_dim)
 
-    def foward(self, img, state):
+    def forward(self, img, state):
 
         # imagen
         x = F.relu(self.conv1(img))
@@ -38,7 +46,7 @@ class QNetwork(nn.Module):
 
         combined = torch.cat([x, s], dim=1)
 
-        f = F.relu(self.fc1(combined))
+        h = F.relu(self.fc1(combined))
         q_values = self.out(h)
 
         return q_values
